30c1da4c92f07798b4ec2ed4359ab0b9b5fe3438
AnonymouX47 <anonymoux47@gmail.com>
2022-10-08 19:43:07 +0100

    [utils] Added integer array sum utilities
    - Add: Defined functions (in "mat47/utils.c") to calculate the sum of an array of integers, for every standard integer type.
    - Add: Defined `sum` function macro (in "mat47/utils.h") to provide a generic interface to all integer array sum functions.

--- src/mat47/utils.c
+++ src/mat47/utils.c
@@ -8,3 +8,18 @@
 
 _Bool MAT47_DEBUG;
 FILE *MAT47_LOG_FILE = NULL;
+
+#define _sum(n, arr) \
+    intmax_t sum = 0; \
+    while (n--) sum += arr[n]; \
+    return sum;
+
+intmax_t sum8(unsigned int n, int8_t arr[]) {_sum(n, arr)}
+intmax_t sum16(unsigned int n, int16_t arr[]) {_sum(n, arr)}
+intmax_t sum32(unsigned int n, int32_t arr[]) {_sum(n, arr)}
+intmax_t sum64(unsigned int n, int64_t arr[]) {_sum(n, arr)}
+
+uintmax_t usum8(unsigned int n, uint8_t arr[]) {_sum(n, arr)}
+uintmax_t usum16(unsigned int n, uint16_t arr[]) {_sum(n, arr)}
+uintmax_t usum32(unsigned int n, uint32_t arr[]) {_sum(n, arr)}
+uintmax_t usum64(unsigned int n, uint64_t arr[]) {_sum(n, arr)}
--- src/mat47/utils.h
+++ src/mat47/utils.h
@@ -4,6 +4,7 @@
  * See https://github.com/AnonymouX47/mat47/LICENCE for license information.
  */
 
+#include <stdint.h>
 #include <stdio.h>
 
 #ifndef MAT47_UTILS
@@ -17,7 +18,28 @@
     )\
 )
 
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#define imax(a, b) (a = max(a, b))
+
+#define sum(n, arr) \
+    _Generic( \
+        (arr), \
+        int8_t *: sum8, int16_t *: sum16, int32_t *: sum32, int64_t *: sum64, \
+        uint8_t *: usum8, uint16_t *: usum16, uint32_t *: usum32, uint64_t *: usum64 \
+    )(n, arr)
+
 extern _Bool MAT47_DEBUG;
 extern FILE *MAT47_LOG_FILE;
+
+intmax_t sum8(unsigned int n, int8_t arr[]);
+intmax_t sum16(unsigned int n, int16_t arr[]);
+intmax_t sum32(unsigned int n, int32_t arr[]);
+intmax_t sum64(unsigned int n, int64_t arr[]);
+
+uintmax_t usum8(unsigned int n, uint8_t arr[]);
+uintmax_t usum16(unsigned int n, uint16_t arr[]);
+uintmax_t usum32(unsigned int n, uint32_t arr[]);
+uintmax_t usum64(unsigned int n, uint64_t arr[]);
 
 #endif
