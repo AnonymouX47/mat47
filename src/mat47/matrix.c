a984fb96629ebf873e5e45d52551d3f504128d00
AnonymouX47 <anonymoux47@gmail.com>
2022-10-08 20:52:03 +0100

    [matrix] Added matrix printing/writing support
    - Add: Added `mat47_fprintf()` to write a formatted string representation of a matrix to a stream, defined in "mat47/matrix.c".
    - Add: Defined `MAT47_ELEM_FMT` macro constant, in "mat47/matrix.h".
    - Add: Defined `mat47_fprint()`, `mat47_printf()` and `mat47_print()` function macros (in "mat47/matrix.h") for extra printing/writing support.

--- src/mat47/matrix.c
+++ src/mat47/matrix.c
@@ -4,7 +4,10 @@
  * See https://github.com/AnonymouX47/mat47/LICENCE for license information.
  */
 
+#include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "matrix.h"
 #include "utils.h"
@@ -69,4 +72,89 @@ void *mat47_del(mat47_t *m)
         free(m);
         mat47_log("Deallocated matrix @ %p", m);
     }
+}
+
+
+#define ELEM_MAX_LEN 24
+#define ELEM_MAX_LEN_NDIGITS 2
+
+intmax_t mat47_fprintf(mat47_t *m, FILE *restrict stream, const char *restrict format)
+{
+    if (!(m && stream && format)) {
+        mat47_log("Null pointer(s): m=%p, stream=%p, format=%p", m, stream, format);
+        return -1;
+    }
+
+    double *restrict row, **restrict data = m->data;
+    unsigned int i, j, n_rows = m->n_rows, n_cols = m->n_cols;
+    char (*row_str)[ELEM_MAX_LEN + 1],  // 1 = '\0'
+         mat_str[n_rows][n_cols][ELEM_MAX_LEN + 1],  // 1 = '\0'
+         // 6 = '|' + ' ' + '%' + 's' + ' ' + '\0'
+         col_fmt[n_cols][ELEM_MAX_LEN_NDIGITS + 6];
+    unsigned char col_widths[n_cols];
+    intmax_t n_bytes = 0;
+
+    mat47_log("Printing matrix @ %p", m);
+
+    memset(col_widths, 0, n_cols * sizeof(*col_widths));
+    for (i = 0; i < n_rows; i++) {
+        row = data[i];
+        row_str = mat_str[i];
+        for (j = 0; j < n_cols; j++)
+            imax(
+                col_widths[j],
+                min(
+                    ELEM_MAX_LEN,
+                    snprintf(row_str[j], ELEM_MAX_LEN + 1, format, row[j])
+                )
+            );
+    }
+
+    mat47_log("Formatted elements");
+    mat47_log(
+        "[1, 1] = %s, [%d, %d] = %s",
+        mat_str[0][0], n_rows, n_cols, mat_str[n_rows - 1][n_cols - 1]
+    );
+
+    for (j = 0; j < n_cols; j++)
+        sprintf(col_fmt[j], "| %%%ds ", col_widths[j]);
+
+    mat47_log("Created column format strings");
+    mat47_log(
+        "[1] = %s, [%d] = %s",
+        col_fmt[0], n_cols, col_fmt[n_cols - 1]
+    );
+
+    // First 3 = '|'/'+' + 2 * ' ' (per column)
+    // Second 3 = '|'/'+' + "\n\0" (at the end of every line)
+    unsigned int line_length = sum(n_cols, col_widths) + 3 * n_cols + 3;
+    char bar[line_length], mid_bar[line_length], *line;
+
+    line = bar;
+    *line++ = '+';
+    // 4 = 2 * '+' + "\n\0"
+    memset(line, '-', line_length - 4); line += line_length - 4;
+    *line++ = '+';
+    strcpy(line, "\n\0");
+
+    line = mid_bar;
+    *line++ = '|';
+    for (j = 0; j < n_cols; j++) {
+        // 2 = 2 * ' ' (surrounding every **formatted** element)
+        memset(line, '-', col_widths[j] + 2); line += col_widths[j] + 2;
+        *line++ = '+';
+    }
+    strcpy(line - 1, "|\n\0");  // Overwrites the last '+' (line - 1)
+
+    n_bytes += fprintf(stream, bar);
+    for (i = 0; i < n_rows; i++) {
+        row_str = mat_str[i];
+        for (j = 0; j < n_cols; j++)
+            n_bytes += fprintf(stream, col_fmt[j], row_str[j]);
+        n_bytes += fprintf(stream, "|\n");
+        if (i < n_rows - 1) n_bytes += fprintf(stream, mid_bar);
+    }
+    n_bytes += fprintf(stream, bar);
+
+    return n_bytes;
 }
--- src/mat47/matrix.h
+++ src/mat47/matrix.h
@@ -4,10 +4,23 @@
  * See https://github.com/AnonymouX47/mat47/LICENCE for license information.
  */
 
+#include <stdint.h>
 #include <stdio.h>
 
 #ifndef MAT47
 #define MAT47
+
+/* The default element format specifier */
+#define MAT47_ELEM_FMT "%.4g"
+
+/* Like `mat47_fprintf()` but with *format* set to `MAT47_ELEM_FMT` */
+#define mat47_fprint(m, stream) mat47_fprintf(m, stream, MAT47_ELEM_FMT)
+
+/* Like `mat47_fprintf()` but with *stream* set to `stdout` */
+#define mat47_printf(m, format) mat47_fprintf(m, stdout, format)
+
+/* Like `mat47_printf()` but with *format* set to `MAT47_ELEM_FMT` */
+#define mat47_print(m) mat47_printf(m, MAT47_ELEM_FMT)
 
 /* Enables/Disables library-wide debugging (disabled by default). */
 extern _Bool MAT47_DEBUG;
@@ -38,6 +51,28 @@ typedef struct {
  * NOTE: This must be called to deallocate the memory internally used by a matrix.
  */
 void *mat47_del(mat47_t *m);
+
+/* Writes a string representation of a matrix to a stream.
+ *
+ * PARAMETERS:
+ * - m: The matrix whose representation should be written
+ * - stream: A narrow-oriented stream to which the matrix representation should be
+ *   written
+ * - format: A valid format specifier for a `double`, to be applied to every element
+ *
+ * RETURN VALUE:
+ * - `-1`, if any argument is null.
+ * - Otherwise, the amount of characters written to the stream.
+ *
+ * NOTE:
+ * - The string representation of every element has a length limit of 24 characters.
+ * - If *format* is invalid for a `double` or not null-terminated, the behaviour is
+ *   undefined.
+ * - If the number of bytes written is greater than INTMAX_MAX, the return value is
+ *   undefined.
+ * - *stream* is not explicitly flushed.
+ */
+intmax_t mat47_fprintf(mat47_t *m, FILE *restrict stream, const char *restrict format);
 
 /* Creates a new zero matrix.
  *
