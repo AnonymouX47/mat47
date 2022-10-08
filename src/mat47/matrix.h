e9077acc30b3d0063926375211f000f13e55952c
AnonymouX47 <anonymoux47@gmail.com>
2022-10-08 22:32:32 +0100

    [docs] Switched to google-style docstrings
    - Change: Preparing documentation comments for Sphynx autodoc via *napoleon*.
    - Change: Updated some function descriptions.

--- src/mat47/matrix.h
+++ src/mat47/matrix.h
@@ -25,9 +25,11 @@
 /* Enables/Disables library-wide debugging (disabled by default). */
 extern _Bool MAT47_DEBUG;
 
-/* Stream to which libray logs should be written. If NULL, logs are written to `stderr`.
+/* Stream to which libray logs should be written.
+ * If null (default), logs are written to `stderr`.
  *
- * NOTE: Only used when debugging is enabled via `MAT47_DEBUG`.
+ * Note:
+ *     Only used when debugging is enabled via `MAT47_DEBUG`.
  */
 extern FILE *MAT47_LOG_FILE;
 
@@ -38,52 +40,54 @@ typedef struct {
     double **data;
 } mat47_t;
 
-/* Deallocates a matrix.
+/* Deallocates memory used by a matrix.
  *
- * PARAMETERS:
- * - m: The matrix to be deallocated
+ * Args:
+ *     m: The matrix to be deallocated
  *
- * RETURN VALUE:
- * - A null pointer, if either argument equals zero or a failure occurs during memory
- *   allocation.
- * - Otherwise, a pointer to a newly allocated matrix.
+ * Returns:
+ *     - A null pointer, if either argument equals zero or a failure occurs during
+ *       memory allocation.
+ *     - Otherwise, a pointer to a newly allocated matrix.
  *
- * NOTE: This must be called to deallocate the memory internally used by a matrix.
+ * Note:
+ *     This must be called to deallocate memory internally used by a matrix.
  */
 void *mat47_del(mat47_t *m);
 
 /* Writes a string representation of a matrix to a stream.
  *
- * PARAMETERS:
- * - m: The matrix whose representation should be written
- * - stream: A narrow-oriented stream to which the matrix representation should be
- *   written
- * - format: A valid format specifier for a `double`, to be applied to every element
+ * Args:
+ *     m: The matrix whose representation should be written
+ *     stream: A narrow-oriented stream to which the matrix representation should be
+ *       written
+ *     format: A valid format specifier for a `double`, to be applied to every element
  *
- * RETURN VALUE:
- * - `-1`, if any argument is null.
- * - Otherwise, the amount of characters written to the stream.
+ * Returns:
+ *     - `-1`, if any argument is null.
+ *     - Otherwise, the amount of characters written to the stream.
  *
- * NOTE:
- * - The string representation of every element has a length limit of 24 characters.
- * - If *format* is invalid for a `double` or not null-terminated, the behaviour is
- *   undefined.
- * - If the number of bytes written is greater than INTMAX_MAX, the return value is
- *   undefined.
- * - *stream* is not explicitly flushed.
+ * Note:
+ *     - The string representation of each element has a length limit of 24 characters.
+ *       Beyond that, an element's string representation is simply truncated.
+ *     - If *format* is invalid for a `double` or not null-terminated, the behaviour is
+ *       undefined.
+ *     - If the number of bytes written is greater than INTMAX_MAX, the return value is
+ *       undefined.
+ *     - *stream* is not explicitly flushed.
  */
 intmax_t mat47_fprintf(mat47_t *m, FILE *restrict stream, const char *restrict format);
 
 /* Creates a new zero matrix.
  *
- * PARAMETERS:
- * - n_rows: Number of rows; 1 <= n_rows <= UINT_MAX
- * - n_cols: Number of columns; 1 <= n_cols <= UINT_MAX
+ * Args:
+ *     n_rows: Number of rows; 1 <= n_rows <= UINT_MAX
+ *     n_cols: Number of columns; 1 <= n_cols <= UINT_MAX
  *
- * RETURN VALUE:
- * - A null pointer, if either argument equals zero or a failure occurs during memory
- *   allocation.
- * - Otherwise, a pointer to a newly allocated matrix.
+ * Returns:
+ *     - A null pointer, if either argument equals zero or a failure occurs during
+ *       memory allocation.
+ *     - Otherwise, a pointer to a newly allocated matrix.
  */
 mat47_t *mat47_new(unsigned int n_rows, unsigned int n_cols);
 
