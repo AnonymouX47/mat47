4d58412bb304362fa07a842cc9b245f250e044b8
AnonymouX47 <anonymoux47@gmail.com>
2022-10-10 20:08:10 +0100

    [utils] Turned `[u]sum*` functions `static`
    - Change: Moved `[u]sum*` functions into "mat47/utils.h" and are now internally linked.
    
    The reason for this is to avoid possible collision with functions in user code.
    Yes, this probably causes the functions to be compiled separately per translation unit in
    which they're used but this seems to be better than the alternatives [I could think of].

--- src/mat47/utils.h
+++ src/mat47/utils.h
@@ -32,14 +32,19 @@
 extern _Bool MAT47_DEBUG;
 extern FILE *MAT47_LOG_FILE;
 
-intmax_t sum8(unsigned int n, int8_t arr[]);
-intmax_t sum16(unsigned int n, int16_t arr[]);
-intmax_t sum32(unsigned int n, int32_t arr[]);
-intmax_t sum64(unsigned int n, int64_t arr[]);
+#define _sum(n, arr) \
+    intmax_t sum = 0; \
+    while (n--) sum += arr[n]; \
+    return sum;
 
-uintmax_t usum8(unsigned int n, uint8_t arr[]);
-uintmax_t usum16(unsigned int n, uint16_t arr[]);
-uintmax_t usum32(unsigned int n, uint32_t arr[]);
-uintmax_t usum64(unsigned int n, uint64_t arr[]);
+static intmax_t sum8(unsigned int n, int8_t arr[]) {_sum(n, arr)}
+static intmax_t sum16(unsigned int n, int16_t arr[]) {_sum(n, arr)}
+static intmax_t sum32(unsigned int n, int32_t arr[]) {_sum(n, arr)}
+static intmax_t sum64(unsigned int n, int64_t arr[]) {_sum(n, arr)}
+
+static uintmax_t usum8(unsigned int n, uint8_t arr[]) {_sum(n, arr)}
+static uintmax_t usum16(unsigned int n, uint16_t arr[]) {_sum(n, arr)}
+static uintmax_t usum32(unsigned int n, uint32_t arr[]) {_sum(n, arr)}
+static uintmax_t usum64(unsigned int n, uint64_t arr[]) {_sum(n, arr)}
 
 #endif  // MAT47_UTILS_H
