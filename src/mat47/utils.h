1aefb7f23c3cd793eb159db63628e5ea7c3a3e88
AnonymouX47 <anonymoux47@gmail.com>
2022-11-07 10:03:44 +0100

    [lib] Code cleanup for [u]sum* utility functions

--- src/mat47/utils.h
+++ src/mat47/utils.h
@@ -76,19 +76,21 @@ extern FILE *MAT47_LOG_FILE;
 extern _Thread_local struct timespec log_timespec;
 extern _Thread_local char time_str[9];
 
-#define _sum(n, arr) \
+#define _sum \
     intmax_t sum = 0; \
     while (n--) sum += arr[n]; \
     return sum;
 
-static intmax_t sum8(unsigned int n, int8_t arr[]) {_sum(n, arr)}
-static intmax_t sum16(unsigned int n, int16_t arr[]) {_sum(n, arr)}
-static intmax_t sum32(unsigned int n, int32_t arr[]) {_sum(n, arr)}
-static intmax_t sum64(unsigned int n, int64_t arr[]) {_sum(n, arr)}
+static intmax_t sum8(unsigned int n, int8_t arr[]) {_sum}
+static intmax_t sum16(unsigned int n, int16_t arr[]) {_sum}
+static intmax_t sum32(unsigned int n, int32_t arr[]) {_sum}
+static intmax_t sum64(unsigned int n, int64_t arr[]) {_sum}
 
-static uintmax_t usum8(unsigned int n, uint8_t arr[]) {_sum(n, arr)}
-static uintmax_t usum16(unsigned int n, uint16_t arr[]) {_sum(n, arr)}
-static uintmax_t usum32(unsigned int n, uint32_t arr[]) {_sum(n, arr)}
-static uintmax_t usum64(unsigned int n, uint64_t arr[]) {_sum(n, arr)}
+static uintmax_t usum8(unsigned int n, uint8_t arr[]) {_sum}
+static uintmax_t usum16(unsigned int n, uint16_t arr[]) {_sum}
+static uintmax_t usum32(unsigned int n, uint32_t arr[]) {_sum}
+static uintmax_t usum64(unsigned int n, uint64_t arr[]) {_sum}
+
+#undef _sum
 
 #endif  // MAT47_UTILS_H
